fec6a3b9301e505b8cc9f2727cbe7278f6688530
lucasnicoa <19623218@std.stei.itb.ac.id>
2025-05-22 22:35:07 +0700

    add Praktikum 5

---
+++ Praktikum 5/1/Couple.java
@@ -0,0 +1,42 @@
+class Couple<K,V>{
+    private K key;
+    private V value;
+
+    public Couple (K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey(){
+        return this.key;
+    }
+
+    public V getVal(){
+        return this.value;
+    }
+
+    public void setKey(K key){
+        this.key = key;
+    }
+
+    public void setVal(V val){
+        this.value = val;
+    }
+
+    public boolean isEqual(Couple c) {
+        return (this.key == c.key && this.value == c.value);
+    }
+
+    public int nearEQ (Couple couple) {
+        if (this.key != couple.key && this.value != couple.value){
+            return 0;
+        }
+        else if (this.key == couple.key && this.value != couple.value){
+            return 1;
+        }
+        else if (this.key != couple.key && this.value == couple.value){
+            return 2;
+        }
+        else return 3;
+    }
+}
